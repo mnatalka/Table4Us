7c7375df4e7cda866a8988f3a031f3c8f2e2ce07
Natalia Maciejowska <natalia.maciejowska@gmail.com>
2015-02-05 10:25:19 +0000

    Calculating global mean for ratings matrix

---
+++ R/user_item_baseline.r
@@ -0,0 +1,6 @@
+get_global_mean <- function(ratings_matrix) {
+
+	global_mean <- mean(as.matrix(ratings_matrix), na.rm = TRUE)
+
+}
+
--- baseline_predictor.r
+++ baseline_predictor.r
@@ -1,4 +1,8 @@
 library("jsonlite")
-source("R/ratings.r")
 
-ratings <- create_ratings_matrix()
+source("R/create_ratings_matrix.r")
+source("R/user_item_baseline.r")
+
+#ratings <- create_ratings_matrix()
+
+global_mean <- get_global_mean(ratings)
